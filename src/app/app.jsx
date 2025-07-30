f5f3aca714ca5a9cba965b8c94db1c9e5b16adb3
Farthy <senelwafarthy@gmail.com>
2025-07-30 17:44:28 +0300

    updates

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,29 +1,24 @@
 "use client";
 import React from "react";
 import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import RTL from "@/app/(DashboardLayout)/layout/shared/customizer/RTL";
-import { ThemeSettings } from "@/utils/theme/Theme";
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeSettings } from '@/utils/theme/Theme';
 import { useSelector } from 'react-redux';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
-import "@/utils/i18n";
+import '@/utils/i18n';
 
 const MyApp = ({ children }) => {
-    const theme = ThemeSettings();
-    const customizer = useSelector((state) => state.customizer);
+  const theme = ThemeSettings();
+  const customizer = useSelector((state) => state.customizer);
 
-    return (
-        <>
-            <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-                <ThemeProvider theme={theme}>
-                    <RTL direction={customizer.activeDir}>
-                        <CssBaseline />
-                        {children}
-                    </RTL>
-                </ThemeProvider>
-            </AppRouterCacheProvider>
-        </>
-    );
+  return (
+    <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </AppRouterCacheProvider>
+  );
 };
 
 export default MyApp;
--- src/store/customizer/CustomizerSlice.jsx
+++ src/store/customizer/CustomizerSlice.jsx
@@ -2,13 +2,13 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   activeDir: 'ltr',
-  activeMode: 'light',
-  activeTheme: 'BLUE_THEME',
+  activeMode: 'light', // This can be light or dark
+  activeTheme: 'BLUE_THEME', // BLUE_THEME, GREEN_THEME, BLACK_THEME, PURPLE_THEME, ORANGE_THEME
   SidebarWidth: 270,
   MiniSidebarWidth: 87,
   TopbarHeight: 70,
-  isLayout: 'boxed',
-  isCollapse: true,
+  isLayout: 'boxed', // This can be full or boxed
+  isCollapse: false, // to make sidebar Mini by default
   isSidebarHover: false,
   isMobileSidebar: false,
   isHorizontal: false,
