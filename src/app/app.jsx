395a06da7433326a78fb828f982ef1e3934b4bbf
Farthy <senelwafarthy@gmail.com>
2025-07-30 18:06:47 +0300

    updda

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from '@mui/material/CssBaseline';
+import RTL from '@/app/(DashboardLayout)/layout/shared/customizer/RTL';
 import { ThemeSettings } from '@/utils/theme/Theme';
 import { useSelector } from 'react-redux';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
@@ -12,12 +13,16 @@ const MyApp = ({ children }) => {
   const customizer = useSelector((state) => state.customizer);
 
   return (
-    <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        {children}
-      </ThemeProvider>
-    </AppRouterCacheProvider>
+    <>
+      <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+        <ThemeProvider theme={theme}>
+          <RTL direction={customizer.activeDir}>
+            <CssBaseline />
+            {children}
+          </RTL>
+        </ThemeProvider>
+      </AppRouterCacheProvider>
+    </>
   );
 };
 
